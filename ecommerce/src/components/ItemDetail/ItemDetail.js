2a91fc34b8745754d6e48249fe009e2cc2e23c1b
AgustinBeker <agustin2012beker@gmail.com>
2023-06-15 19:16:27 -0300

    Revert "Cambios (vuelta atras?)"
    This reverts commit 754f53dbd6bffe6598c4d288367ea74fe4412344.

--- ecommerce/src/components/ItemDetail/ItemDetail.js
+++ ecommerce/src/components/ItemDetail/ItemDetail.js
@@ -1,26 +1,7 @@
 import './ItemDetail.css'
-import { useContext, useState } from 'react'
 import ItemCount from '../ItemCount/ItemCount'
-import { Link } from 'react-router-dom'
-
-import { CartContext } from '../context/CartContext'
 
 const ItemDetail = ({id, name, img, category, description, price, stock}) => {
-    const [quantityAdded, setQuantityAdded] = useState(0)
-
-    const { addItem } = useContext(CartContext)
-
-    const handleOnAdd = (quantity) => {
-        setQuantityAdded(quantity)
-
-        const item ={
-            id, name, price
-        }
-
-        addItem(item, quantity)
-
-    }
-
     return (
         <article className="CardItem">
             <header className="Header">
@@ -43,13 +24,7 @@ const ItemDetail = ({id, name, img, category, description, price, stock}) => {
                 </p>
             </section>
             <footer className='ItemFooter'>
-                {
-                    quantityAdded > 0 ? (
-                        <Link to='/cart' className='Option'>Terminar compra</Link>
-                    ) : (
-                        <ItemCount initial={1} stock={stock} onAdd={handleOnAdd}/>
-                    )
-                }
+            <ItemCount initial={1} stock={10} onAdd={(quantity) => console.log('Cantidad agregada ',quantity)}/>
             </footer>
         </article>
     )
